28fdfedeea1fc583a8355da57532f76b7776bcf4
ilanyaniv <ilan.yaniv@gmail.com>
2020-12-13 17:31:55 +0200

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const fetch = require('node-fetch');
-const {getAllServices} = require('./autocomplete');
+const {getAllServices, getUserList} = require('./autocomplete');
 
 ///////////////////// METHODS ///////////////////// 
 async function createNewIncident(action,settings) {
     const method = "POST";
     const url = "https://api.pagerduty.com/incidents";
     const serviceId = action.params.SERVICE_ID.id;
-    const title = action.params.TITLE
+    const userId = action.params.ASSIGNEE.id;
+    const title = action.params.TITLE;
+    const email = action.params.EMAIL;
     let body = {
         "incident": {
             "type": "incident",
@@ -17,25 +19,42 @@ async function createNewIncident(action,settings) {
             }
         }
     }
+    if (typeof (userId) != 'undefined') {
+        const a = {
+            "id": `${userId}`,
+            "type": "user_reference"
+        }
+        body.assignments = [a];
+    }
     body = JSON.stringify(body);
-    return await genericRestAPI(action,settings, method, url, body);
+    return await genericRestAPI(action,settings, method, url, body, email);
+}
+
+async function createChangeEvent (action, settings) {
+    const method = "POST";
+    const url = "https://api.pagerduty.com/change_events";
+    return await genericRestAPI(action,settings, method, url);
 }
 
 ///////////////////// HELPERS ///////////////////// 
-async function genericRestAPI(action, settings, method, url, body) {
+async function genericRestAPI(action, settings, method, url, body, email) {
     /**
      * Send Default API Request
      */
-    const request = {
+    let request = {
         method: `${method}`,
         headers: {
             'Authorization': `Token token=${settings.TOKEN}`,
             'Accept': 'application/vnd.pagerduty+json;version=2',
             'Content-Type': 'application/json',
-            'From': `${action.params.EMAIL}`
         },
         body: body
     };
+
+    if (typeof(email) !== 'undefined') {
+        request.headers.From = `${action.params.EMAIL}`
+    }
+    
     response = await fetch(url, request);
     if (!response.ok) {
         throw response
@@ -45,5 +64,8 @@ async function genericRestAPI(action, settings, method, url, body) {
 
 module.exports = {
     CREATE_INCIDET : createNewIncident,
-    getAllServices
-}
+    CREATE_CHANGE_EVENT: createChangeEvent,
+    // autocomplete
+    getAllServices,
+    getUserList
+};
--- src/autocomplete.js
+++ src/autocomplete.js
@@ -1,5 +1,9 @@
 const fetch = require('node-fetch');
+
 async function getAllServices(query, pluginSettings) {
+    /**
+     * This method will return to the plugin all the existing services to attach to the created incident.
+     */
     const url = "https://api.pagerduty.com/services?limit=25?total=true&time_zone=UTC&sort_by=name";
     const request = {
         method: 'GET',
@@ -20,6 +24,40 @@ async function getAllServices(query, pluginSettings) {
     }
     return options.filter(option => option.value.includes(query));
 }
+
+async function getUserList(query, pluginSettings) {
+    /**
+     * This method will return to the plugin all the existing users to assign to the new incidet
+     */
+    const url = "https://api.pagerduty.com/users";
+    const request = {
+        method: 'GET',
+        headers: {
+            'Authorization': `Token token=${pluginSettings[0].value}`,
+            'Accept': 'application/vnd.pagerduty+json;version=2',
+            'Content-Type': 'application/json',
+        }   
+    }
+    const response = await fetch(url, request);
+    if (!response.ok) {
+        throw response
+    }
+    const result = await response.json();
+    let options = result.users.map((user) => ({ id: user.id, value: user.name}));
+    if (!query) {
+        return options;
+    }
+    return options.filter(option => option.value.includes(query));
+}
+
+var pluginSettings = [
+    {
+        value:"HE851c5mKMxfm7SA-W5y"
+    }
+]
+var query = ""
+getUserList (query, pluginSettings)
 module.exports = {
-    getAllServices
+    getAllServices,
+    getUserList
 }
